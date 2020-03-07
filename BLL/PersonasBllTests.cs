2cc0bdab52f7988d47f3d644ff011372a5ecd84f
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-03-07 13:50:20 -0400

    Registro Con Detalle

---
+++ BLL/PersonasBllTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegistroConDetalle.BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistroConDetalle.BLL.Tests
+{
+    [TestClass()]
+    public class PersonasBllTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
